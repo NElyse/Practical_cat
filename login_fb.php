5e4019d9c21066023c3ab5c066bb463aa35776f4
NElyse <elinsengimana@gmail.com>
2022-06-30 18:25:38 +0200

    log mochup

---
+++ login_fb.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login with face book account</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="boostrap\css\bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/boostrap\js\bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <form action="login-fb.php">
+
+    <header class="text-white p-1 text-center" style="background-color:blue;">
+        <h6>IREMBO Practical CAT</h6>
+    </header>
+
+    <div class="d-flex justify-content-center pt-5 align-items-center my-3">
+        <div class="border my-3 px-3 pt-5 position-relative text-center w-40 " style="height:60vh; width: 45vh;">
+
+            <p>
+                <b>Connect to IREMBO</b>
+            </p>
+
+            <div class="position-relative" style="top: 60px";>
+                <button type="button" class=" text-white fw-light w-100 text-start btn-sm" style="background-color:blue;">
+                    <i style="font-size: 25px;" class="fa fa-facebook-square" aria-hidden="true"></i>
+                    <span class="ps-2"> Countinue with facebook</span>
+                    
+                </button>
+                
+
+            </div>
+        </div>
+
+    </div>
+
+</form>
+</body>
+
+</html>
